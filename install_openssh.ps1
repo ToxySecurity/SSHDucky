dd6b819b99e9f652a95f42e854b4a16eec173123
Toxy Security <chiranjiv2904@gmail.com>
2023-06-15 12:17:51 +0530

    Updated

--- install_openssh.ps1
+++ install_openssh.ps1
@@ -1,5 +1,5 @@
 # Download the OpenSSH installer
-$opensshInstallerURL = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.2.2.0p1-Beta/OpenSSH-Win32-v9.2.2.0.msi"
+$opensshInstallerURL = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.2.2.0p1-Beta/OpenSSH-Win64-v9.2.2.0.msi"
 $opensshInstallerPath = "$PWD\OpenSSHInstaller.msi"
 
 Write-Host "Downloading OpenSSH installer..."
@@ -14,15 +14,18 @@ if (Test-Path $opensshInstallerPath) {
     Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$opensshInstallerPath`" /qn" -Wait
 
     # Check if the installation was successful
-    $opensshPath = "C:\Program Files\OpenSSH"
+    $isOpenSSHInstalled = Get-WindowsCapability -Online | Where-Object { $_.Name -like 'OpenSSH*' }
 
-    if (Test-Path $opensshPath) {
+    if ($isOpenSSHInstalled) {
+        Write-Host "OpenSSH installation completed."
         exit 0
     }
     else {
+        Write-Host "OpenSSH installation failed."
         exit 1
     }
 }
 else {
+    Write-Host "OpenSSH installer download failed."
     exit 1
 }
